b838717c7d2c1c5a37c0b9ca3b19e2862f381ea6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-06-26 14:08:38 -0400

    Create csvParser.js
    File parses an inputted csv

---
+++ scripts/ElectionBehavior/csvParser.js
@@ -0,0 +1,19 @@
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    displayContents(contents);
+  };
+  reader.readAsText(file);
+}
+
+function displayContents(contents) {
+  console.log(contents.split("\n"))
+}
+
+document.getElementById('fileInput')
+  .addEventListener('change', readSingleFile, false);
