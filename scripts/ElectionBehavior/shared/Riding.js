3f8922a03835129c39d2687ecb8a82d8c9f5c729
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-05 19:09:08 -0400

    Update Riding.js

--- scripts/ElectionBehavior/shared/Riding.js
+++ scripts/ElectionBehavior/shared/Riding.js
@@ -6,8 +6,213 @@ class Riding
 	#name;
 	#population;
 	#startTime;
-	#setSize;
+	#deltaTime;
 	
-	#candidate = []
+	#candidates = []
+
+
+	/**
+	 * constructor assigns and validates new candidate values
+	 * 
+	 * @param {Array} arguments[0] array of all values within riding.csv
+	 *
+	 * @param {String} arguments[0] name of riding
+	 * @param {int} arguments[1] population of riding
+	 * @param {float} arguments[2] the time in which the riding polling should start counting votes
+	 * @param {float} arguments[3] the time between each polling step
+	 *
+	 * @throw {IlligalArguments} throws if the wrong number of IlligalArguments were given
+	 */
+	constructor()
+	{
+		if (arguments.length === 1)
+		{
+			this.#constructor1(arguments[0]);
+		}
+		else if(arguments.length === 4)
+		{
+			this.#constructor2(arguments[0], arguments[1], arguments[2], arguments[3])
+		}
+		else
+		{
+			throw "IlligalArguments";
+		}
+	}
+
+	/**
+	 * constructor1 assigns and validates new candidate values
+	 * 
+	 * @param {Array} line array of all values within riding.csv
+	 *
+	 * @throw {IlligalArguments} throws line does not have 4 items
+	 */
+	#constructor1(line)
+	{
+		if(line.length != 4)
+		{
+			throw "IlligalArguments"
+		}
+
+		if(!isNaN(parseFloat(line[0])))
+		{
+			this.#startTime = line[0]
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+
+		if(!isNaN(parseFloat(line[1])))
+		{
+			this.#deltaTime = line[1]
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+
+		if(/^\d+$/.test(line[2]))
+		{
+			this.#population = parseInt(line[2])
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+
+		this.#name = line[3]
+	}
+
+	/**
+	 * constructor2 assigns and validates new candidate values
+	 * 
+	 * @param {String} name name of riding
+	 * @param {int} population population of riding
+	 * @param {float} startTime the time in which the riding polling should start counting votes
+	 * @param {float} deltaTime the time between each polling step
+	 *
+	 * @param {IlligalArguments} throws if the wrong data type is inserted
+	 */
+	#constructor2(name, population, startTime, deltaTime)
+	{
+		if(typeof name === "string")
+		{
+			this.#name = name;
+		}
+		else
+		{
+			throw "IlligalArguments";
+		}
+
+		if(/^\d+$/.test(population.toString()))
+		{
+			this.#population = population;
+		}
+		else
+		{
+			throw "IlligalArguments";
+		}
+
+		if(!isNaN(startTime) && typeof startTime === "number")
+		{
+			this.#startTime = startTime;
+		}
+		else
+		{
+			throw "IlligalArguments";
+		}
+
+		if(!isNaN(deltaTime) && typeof deltaTime === "number")
+		{
+			this.#deltaTime = deltaTime;
+		}
+		else
+		{
+			throw "IlligalArguments";
+		}
+	}
+	
+	/**
+	 * addCandidate adds a Candidate Object to #candidates array
+	 * 
+	 * @param {Candidate} candidate object that is added to #candidates array
+	 * @param {Array} candidate array from riding.csv
+	 */
+	addCandidate(candidate)
+	{
+		if(typeof candidate === "object" && candidate.constructor.name === Party.name)
+		{
+			this.#candidates.push(candidate)
+		}
+		else if(Array.isArray(candidate))
+		{
+			var tmp = new Candidate(candidate)
+
+			this.#candidates.push(candidate)
+		}
+	}
+
+	/**
+	 * getName returns the name of riding
+	 *
+	 * @return {string} name of the riding
+	 */
+	getName()
+	{
+		return this.#name
+	}
+
+	/**
+	 * getPopulation returns the number of people in a population
+	 *
+	 * @return {int} name of the riding
+	 */
+	getPopulation()
+	{
+		return this.#population
+	}
+	
+	/**
+	 * getStartTime returns the start time in minutes since the begining of the liveStream
+	 *
+	 * @return {float} the counting start time of the riding
+	 */
+	getStartTime()
+	{
+		return this.#startTime
+	}
+	
+	/**
+	 * getDeltaTime returns the number of seconds between updates
+	 *
+	 * @return {float} the delta time
+	 */
+	getDeltaTime()
+	{
+		return this.#deltaTime
+	}
+	
+	
+	/**
+	 * getCandidate returns the candidates in the riding
+	 *
+	 * @return {Array} the candidates in the riding and their votes
+	 */
+	getCandidateVote(time)
+	{
+		var candidateVote = []
+		for(var i1 = 0; i1 < this.#candidates; i1++)
+		{
+			candidateVote.push({votes: this.#candidates[i1].getVoteCount(time), candiate : this.#candidates[i1]})
+		}
+
+		candidateVote.sort((a, b) => 
+		{
+			return a.votes + a.votes;
+		});
+
+		candidateVote.reverse()
+		return candidateVote
+	}
 
 }
