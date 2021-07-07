274f5e4bad27d14c7ac028c74cf2544dd1e642f6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-07 11:00:49 -0400

    added Setters to Main.js

--- scripts/ElectionBehavior/Main.js
+++ scripts/ElectionBehavior/Main.js
@@ -1,11 +1,87 @@
 class Main
 {
-
 	static parties = []
 	static ridings = []
 
-	static Main()
-	{
+	static active = true;
+	static primaryDuration;
+	static secondaryDuration;
 
+	/**
+	 * addParties appends a new party into parties array
+	 *
+	 * @param {param} party that will be appended to parties
+	 *
+	 * @throw {IlligalArguments} throws when paramaters are not party
+	 */
+	addParties(party)
+	{
+		if(typeof party === "object" && party.constructor.name === Party.name)
+		{
+			Main.parties.push(party)
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
 	}
+
+	/**
+	 * addRiding appends a new riding into ridings array
+	 *
+	 * @param {param} riding that will be appended to ridings
+	 *
+	 * @throw {IlligalArguments} throws when paramaters are not riding
+	 */
+	addRiding(riding)
+	{
+		if(typeof riding === "object" && riding.constructor.name === Riding.name)
+		{
+			Main.ridings.push(riding)
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+	}
+
+	/**
+	 * setPrimaryDuration sets the number of seconds the primary riding vissible before transition to another ridding 
+	 *
+	 * @param {param} duration that will be visible before transition to another riding
+	 *
+	 * @throw {IlligalArguments} throws when paramaters are not int
+	 */
+	setPrimaryDuration(duration)
+	{
+		if(/^\d+$/.test(duration))
+		{
+			Main.primaryDuration = duration
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+	}
+
+	/**
+	 * setSecondaryDuration sets the number of seconds the secondary riding vissible before transition to another ridding 
+	 *
+	 * @param {param} duration that will be visible before transition to another riding
+	 *
+	 * @throw {IlligalArguments} throws when paramaters are not int
+	 */
+	setSecondaryDuration(duration)
+	{
+		if(/^\d+$/.test(duration))
+		{
+			Main.secondaryDuration = duration
+		}
+		else
+		{
+			throw "IlligalArguments"
+		}
+	}
+
+
 }
