00d47dc5e3571b6f0a3dccaa63040134468837a1
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-11 15:54:16 -0400

    added dataType checks in Exception.js

--- scripts/ElectionBehavior/shared/Exception.js
+++ scripts/ElectionBehavior/shared/Exception.js
@@ -2,14 +2,43 @@ class Exception
 {
 	error;
 	message;
+
+	/**
+	 * constructor assigns error code and optional message
+	 *
+	 * @param {string} error name or exception
+	 * @param {string} message is string providing additonal information about the exception 
+	 *
+	 * @throw {Exception} throws when paramaters are strings 
+	 */
 	constructor(error, message = "")
 	{
-		this.error = error
-		this.message = message
+		if(typeof error === "string")
+		{
+			this.error = error
+		}
+		else
+		{
+			throw new Exception("IlligalArguments", "error must be string");
+		}
+
+		if(typeof message === "string")
+		{
+			this.message = message
+		}
+		else
+		{
+			throw new Exception("IlligalArguments", "message must be string");
+		}
 	}
 
+	/**
+	 * toString returns string of exception and message
+	 *
+	 * @return {string} string of exception and message
+	 */
 	toString()
 	{
-		return this.error+":"+this.message
+		return this.error+" : "+this.message
 	}
 }
