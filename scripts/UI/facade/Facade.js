a2540f5dfb0e9efc646d2a01c56fb3c479ac2db2
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-11 22:16:49 -0400

    Create Facade pattern parent class

---
+++ scripts/UI/facade/Facade.js
@@ -0,0 +1,26 @@
+/**
+ * Facade is a abstract Facade patern class
+ */
+class Facade
+{
+	constructor()
+	{
+		throw new Exception("Abstract method not implemented")
+	}
+
+	/**
+	 * draw method creates html elements
+	 */
+	draw()
+	{
+		throw new Exception("Abstract method not implemented")
+	}
+
+	/**
+	 * update method updates the created html elements
+	 */
+	update()
+	{
+		throw new Exception("Abstract method not implemented")
+	}
+}
