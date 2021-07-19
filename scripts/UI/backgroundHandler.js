0c3692d99e069f976b1daf1b16b18a7c56508017
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-19 11:32:36 -0400

    Added scale and translation animation to the background image

--- scripts/UI/backgroundHandler.js
+++ scripts/UI/backgroundHandler.js
@@ -7,6 +7,11 @@ suffix = "jpg"
 current = 0;
 next = 1;
 
+//background start values
+var scale = 1;
+var position = {x: 0, y: 0}
+var transformDuration = 1;
+
 $("#background #currentBackgroundImage").css("transition-duration",(duration*0.25)+"s")
 
 function changeBackground(d)
@@ -88,4 +93,41 @@ function changeBackground(d)
 	)
 }
 
-changeBackground(duration);
+//transitionDuration updates transition-duration of the background image
+function transitionDuration(d)
+{
+	$("#background").css("transition-duration", d+"s")
+	transformDuration = Math.random() * 10 + 5
+	setTimeout
+	(
+		function()
+		{
+			transitionDuration(transformDuration)
+		},
+		d * 1000
+	)
+}
+
+//backgroundTransform updates the scale and translation of the background image
+function backgroundTransform(d)
+{
+	scale = Math.random() / 4 + 1
+
+	position.x = Math.random() * -29.4117647059
+	position.y = Math.random() * (window.innerHeight * -0.15) / window.innerHeight * 100
+
+	$("#background").css("transform", "scale("+scale+") translate("+position.x+"%,"+position.y+"%)")
+
+	setTimeout
+	(
+		function()
+		{
+			backgroundTransform(transformDuration + 1)
+		},
+		d * 1000
+	)
+}
+
+changeBackground(transformDuration);
+backgroundTransform(transformDuration)
+transitionDuration(transformDuration)
