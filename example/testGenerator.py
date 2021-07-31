d7a40693088e35aa7cf6a3c32714847850fb28a7
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-31 09:53:46 -0400

    Create Party class in testGenerator.py

--- example/testGenerator.py
+++ example/testGenerator.py
@@ -1,1 +1,10 @@
 
+class Party:
+    def __init__(self, key, shortName, longName, colour):
+        self.key = key
+        self.shortName = shortName
+        self.longName = longName
+        self.colour = colour
+    def __str__(self):
+        return "{0}\t{1}\t{2}\t{3}".format(self.key, self.shortName, self.longName, self.colour)
+    
