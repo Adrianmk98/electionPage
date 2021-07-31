435ec77836c97ab16f640d6ad7727c5738cf06ba
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-31 09:54:07 -0400

    Create Candidate class in testGenerator.py

--- example/testGenerator.py
+++ example/testGenerator.py
@@ -8,3 +8,15 @@ class Party:
     def __str__(self):
         return "{0}\t{1}\t{2}\t{3}".format(self.key, self.shortName, self.longName, self.colour)
     
+class Candidate:
+    def __init__(self, partyKey, faceSteal, name):
+        self.partyKey = partyKey
+        self.faceSteal = faceSteal
+        self.name = name
+        self.polling = []
+    def __str__(self):
+        tmp = "{0}\t{1}\t{2}".format(self.partyKey, self.faceSteal, self.name)
+        for poll in self.polling:
+            tmp += "\t{0}".format(poll)
+        return tmp
+    
