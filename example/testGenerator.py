c1f6bbd130018b9480f5df627bc78fb80855e75a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-07-31 22:42:44 -0400

    Added docstring

--- example/testGenerator.py
+++ example/testGenerator.py
@@ -2,19 +2,52 @@ import random
 import math
 
 class Party:
+    '''
+        Party class stores key aspects of a Poltical Party
+    '''
+    
     def __init__(self, key, shortName, longName, colour):
+        '''
+            __init__ assigns values for key poltical party names and values
+
+            Parameters:
+                key (String): key is a short unique name assoicated with party
+                shortName (String): shortName is a abbreviation of a poltical party
+                longName (String): longName is the name of a poltical party
+                colour (String): colour is the party colour
+        '''
         self.key = key
         self.shortName = shortName
         self.longName = longName
         self.colour = colour
+        
     def __str__(self):
+        '''
+            __str__ returns a string of key Party instances
+
+            Returns:
+                String of key Party instances
+        '''
         return "{0}\t{1}\t{2}\t{3}".format(self.key, self.shortName, self.longName, self.colour)
     
 class Candidate:
-    def __init__(self, partyKey, faceSteal, name):
+    '''
+        Candidate class stores key aspects of a candidate
+    '''
+     
+    names = None
+    
+    def __init__(self, partyKey, faceSteal, name = None):
+        '''
+            __init__ assigns values for candidate
+
+            Parameters:
+                partyKey (String): unqiue key value from a Party
+                faceSteal (String): image name of faceSteal file
+                name (String): name of candidate
+        '''
         self.partyKey = partyKey
         self.faceSteal = faceSteal
-        self.name = name
         if name == None:
             self.name = Candidate.randomName()
         else:
@@ -22,6 +55,12 @@ class Candidate:
         self.polling = []
         
     def __str__(self):
+        '''
+            __str__ returns a string of key Candidate instances
+
+            Returns:
+                Strings of key Candidate insances
+        '''
         tmp = "{0}\t{1}\t{2}".format(self.partyKey, self.faceSteal, self.name)
         for poll in self.polling:
             tmp += "\t{0}".format(poll)
@@ -40,24 +79,40 @@ class Candidate:
         return Candidate.names[random.randint(0,len(Candidate.names)-1)].replace("\n","")
     
 class Riding:
+    '''
+        Riding Class stores key aspects of a Riding 
+    '''
     def __init__(self, startTime, stepSize, population, ridingName):
+        '''
+            __init__ assigns values for Riding
+
+            Parameters:
+                startTime (int): number of seconds before the riding begins counting votes
+                stepSize (int): number of seconds before the vote checkpoints
+                population (int): number people in a riding
+                ridingName (String): name of riding
+        '''
         self.startTime = startTime
         self.stepSize = stepSize
         self.population = population
         self.ridingName = ridingName
         self.parties = []
-    def generateCandidates(parties, steps):
         
     def generateCandidates(self, parties, steps):
+        '''
+            generateCandidates procedurally creates a set of candidates in the riding
+
+            Parameters:
+                parties (list(Party)): list of Parties to choose Candidate parties
+                steps (int): number of steps/vote checkpoints
+        '''
         partiesCopy = parties.copy()
         candidateParties = []
         #get candidates in riding
-        for i1 in range(random.randint(0, len(parties) - 1)):
         for i1 in range(random.randint(1, len(parties) - 1)):
             candidateParties.append(partiesCopy.pop(random.randint(0,len(partiesCopy)-1)))
         #add candidates in riding
         for party in candidateParties:
-            self.parties.append(Candidate())
             self.parties.append(Candidate(party.key, "1.jpg"))
         #generate polling for candidates
         totalVotes = self.population
@@ -67,16 +122,28 @@ class Riding:
             for candidate in tmpParties:
                 if totalVotes <= 0:
                     break;
-                vote = random.randint(0, totalVotes)
-                if len(candidate.polling) != 0 :
                 vote = Riding.voteFormula(random.random(), 0, totalVotes, (steps - i - 1) / steps)
                 totalVotes -= vote
                 if len(candidate.polling) > 0 :
                     vote += candidate.polling[-1]
                 candidate.polling.append(vote)
             if totalVotes <= 0:
-                break;     
+                break;
+            
     def voteFormula(x, minVote, maxVote, offset = 0, scale = 1):
+        '''
+            voteFormula is a static method that returns number of votes gathered by a candidate at a specfic time based on the formula floor(16*maxVote(x-0)^2(x-1)^2+minVote)
+
+            Parameters:
+                x (float): is a random nuber between 0 - 1
+                minVote (int): is the minimum number of votes that can be retrived
+                maxVote (int): is the maximum number of votes that can be retrived
+                offset (float): is a horizontal offset of a curve
+                scale (float): horizontal scale factor of a curve
+
+            Returns:
+                int number of seats won by candidate
+        '''
         if x < offset * scale or x > (1 + offset) * scale:
             return minVote
         vote = 16 * maxVote * (1 / scale * x - offset)**2 * (1/scale*x-(offset+1))**2 + minVote
@@ -84,16 +151,32 @@ class Riding:
         return vote
     
     def __str__(self):
+        '''
+            __str__ returns a string of key Riding information
+
+            Returns:
+                String of key Riding information
+        '''
         tmp = "{0}\t{1}\t{2}\t{3}".format(self.startTime, self.stepSize, self.population, self.ridingName)
         for party in self.parties:
             tmp += "\n{0}".format(party)
         return tmp
 
-def saveFile(string, name):
-    with open(name, "w") as file:
-        f.write(string)
+def saveFile(content, name):
+    '''
+        saveFile method saves a file
+
+        Parameters:
+            content (String): content is string that is used to fill file
+            name (Stirng): name is name of file
+    '''
+    with open(name+".csv", "w") as file:
+        file.write(content)
         
 def main():
+    '''
+        main method generates parties.csv and riding.csv
+    '''
     partyCount = 5
     
     ridingGroups = 5
@@ -110,7 +193,6 @@ def main():
     hexaDecimal = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
     
     for i in range(partyCount):
-        parties.append(Party("par{0}".format(i + 1), "P{0}".format(i + 1), "party-{0}".format(i + 1), "rgb({0},{1},{2})".format(random.randint(0,255), random.randint(0,255), random.randint(0,255))))
         colour = "#"
         for i2 in range(3):
             colour += "{0}{1}".format(hexaDecimal[random.randint(0,15)],hexaDecimal[random.randint(0,15)])
@@ -120,10 +202,6 @@ def main():
     
     for i1 in range(ridingGroups):
         for i2 in range(ridingCount):
-            ridings.append(Riding(i1 * defaultCountTime, random.randint(*stepSize), random.randint(*populationSize), "Riding-{0}".format(i1*ridingGroups+i2)))
-            ridings[-1].generateCandidates(parties, )
-            print(ridings[-1])
-main()
             ridings.append(Riding(i1 * defaultCountTime * secondsToMin, random.randint(*stepSize) * secondsToMin, random.randint(*populationSize), "Riding-{0}".format(i1*ridingGroups+i2)))
             ridings[-1].generateCandidates(parties, random.randint(5,25))
             
